efc53fa347591c53d0ce2da83e886625d158a300
rama-shararah <115452637+rama-shararah@users.noreply.github.com>
2022-11-06 18:05:00 +0200

    task

---
+++ Nov 6/task2.js
@@ -0,0 +1,10 @@
+let x=1;
+
+for (let i = 1; i <= 4; i++) {
+  
+  for (let j = 1; j <= i; j++) {
+    document.write(x + " ");
+    x++;
+  }
+  document.write("<br>");
+}
---
+++ Nov 6/task4.js
@@ -0,0 +1,9 @@
+let x=0;
+for(let i=0; i<=100; i++){
+   document.write(i + "<br>");
+   x = x + i;
+   
+   if (x > 100){ break;}
+}
+
+

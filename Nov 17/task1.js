74555df9bb74c1f36f35f5bb6c8f43371e4723d1
rama-shararah <115452637+rama-shararah@users.noreply.github.com>
2022-11-18 23:01:43 +0300

    task

--- Nov 17/task1.js
+++ Nov 17/task1.js
@@ -1,22 +1,39 @@
-async function getData(){
-    const responsive= await fetch("https://www.breakingbadapi.com/api/characters");
-    const data= await responsive.json();
-    for(let i=0; i<data.length; i++){
-        document.getElementById("select").innerHTML+=`<option>${data[i].name}</option>`;
-       
-    }
+async function getData() {
+  const responsive = await fetch(
+    "https://www.breakingbadapi.com/api/characters"
+  );
+  const data = await responsive.json();
+  for (let i = 0; i < data.length; i++) {
+    document.getElementById(
+      "select"
+    ).innerHTML += `<option>${data[i].name}</option>`;
+  }
 }
 getData();
 
-async function myfun(){
-    const responsive= await fetch("https://www.breakingbadapi.com/api/characters");
-    const data= await responsive.json();
-    let value= document.getElementById("select").value;
-    for(let x=0;x<data.length;x++){
-        
-        if(value==data[x].name){
-            document.getElementById("img").src=data[x].img;
-
-        }
+async function myfun() {
+  const responsive = await fetch(
+    "https://www.breakingbadapi.com/api/characters"
+  );
+  const data = await responsive.json();
+  let value = document.getElementById("select").value;
+  for (let x = 0; x < data.length; x++) {
+    if (value == data[x].name) {
+      document.getElementById("img").src = data[x].img;
     }
+  }
+}
+
+async function getOnlyWhite() {
+  let p = document.getElementById("txt").value;
+  const responsive = await fetch(
+    `https://www.breakingbadapi.com/api/characters?name=${p}`
+  );
+  const data = await responsive.json();
+  if (data.length < 1) {
+    document.getElementById("img2").src =
+      " https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+  } else {
+    document.getElementById("img2").src = data[0].img;
+  }
 }
